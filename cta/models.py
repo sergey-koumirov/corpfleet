08f8f3dc7abf8759a03dd7b36fa307cac57b8142
Sergey Koumirov <sergey.koumirov@gmail.com>
2016-06-27 18:11:52 +0400

    saving

--- cta/models.py
+++ cta/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class RawData(models.Model):
     raw = models.TextField()
     date = models.DateTimeField()
-    name = models.CharField()
+    name = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return "[{}] {}".format(self.id, self.name)
