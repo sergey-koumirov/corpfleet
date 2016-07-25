948e7fb7b2d3a7b43aaf74a49765c752f05d7e44
sergey.koumirov <sergey.koumirov@gmail.com>
2016-07-25 22:36:12 +0400

    added bwf check

--- cwo/management/commands/sov.py
+++ cwo/management/commands/sov.py
@@ -1,11 +1,25 @@
 from django.core.management.base import BaseCommand, CommandError
+from preston.crest import Preston
+import pprint
 
 class Command(BaseCommand):
     help = 'Update Sov Info'
 
-    def add_arguments(self, parser):
-        parser.add_argument('id', nargs='+', type=int)
-
     def handle(self, *args, **options):
-        for id in options['id']:
-            self.stdout.write(self.style.SUCCESS('ID: "%s"' % id))
+        preston = Preston()
+        structures = preston.sovereignty.structures()
+
+        print('Pages: {}'.format(structures.pageCount))
+        print('Total: {}'.format(structures.totalCount))
+        print('Total: {}'.format(len(structures.items)))
+
+        for structure in structures.items:
+            if structure.solarSystem.name == 'BWF-ZZ':
+                print(structure)
+
+                # bwf = system()
+                # pprint.pprint(bwf.data)
+                #
+                # stats = bwf.stats()
+                # pprint.pprint(stats.data)
+
