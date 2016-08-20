a67c51b4ceab5c7fa9ee39317f297ad804825f5f
sergey.koumirov <sergey.koumirov@gmail.com>
2016-08-20 12:09:36 +0400

    fixed django 10 issue

--- corpfleet/urls.py
+++ corpfleet/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
 
 urlpatterns = [
     url(r'^cta/', include('cta.urls') ),
     url(r'^admin/', admin.site.urls),
     url(r'^wars/', include('cwo.urls')),
     url(r'^$', views.index, name='index' ),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'},  name='login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',  name='logout'),
+    url(r'^accounts/login/$', login, {'template_name': 'admin/login.html'},  name='login'),
+    url(r'^accounts/logout/$', logout,  name='logout'),
 ]
